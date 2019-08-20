c40a54809e027da3073eaa053ea93ee1561b782f
Anel Salas <39442665+anelsalas@users.noreply.github.com>
2019-08-19 20:26:40 -0400

    Added FaceAligner class
    Added FaceAligner class to improve recognition. The facealigner class is f based on the dlib implementation announced here: http://blog.dlib.net/2014/08/real-time-face-pose-estimation.html

--- FaceAligner.py
+++ FaceAligner.py
@@ -2,15 +2,72 @@
 # you can find shape predictor shape_predictor_68_face_landmarks.dat here:
 # http://dlib.net/files/
 
-from .helpers import FACIAL_LANDMARKS_IDXS
-from .helpers import shape_to_np
+#from .helpers import FACIAL_LANDMARKS_IDXS
+#from .helpers import shape_to_np
+from collections import OrderedDict
 import numpy as np
 import cv2
 import dlib
 
+
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
+
+def rect_to_bb(rect):
+	# take a bounding predicted by dlib and convert it
+	# to the format (x, y, w, h) as we would normally do
+	# with OpenCV
+	x = rect.left()
+	y = rect.top()
+	w = rect.right() - x
+	h = rect.bottom() - y
+
+	# return a tuple of (x, y, w, h)
+	return (x, y, w, h)
+
+def shape_to_np(shape, dtype="int"):
+	# initialize the list of (x, y)-coordinates
+	coords = np.zeros((shape.num_parts, 2), dtype=dtype)
+
+	# loop over all facial landmarks and convert them
+	# to a 2-tuple of (x, y)-coordinates
+	for i in range(0, shape.num_parts):
+		coords[i] = (shape.part(i).x, shape.part(i).y)
+
+	# return the list of (x, y)-coordinates
+	return coords
+
  
 class FaceAligner:
-    def __init__(self, predictor, desiredLeftEye=(0.35, 0.35),
+    def __init__(self, desiredLeftEye=(0.35, 0.35),
         desiredFaceWidth=256, desiredFaceHeight=None):
         # store the facial landmark predictor, desired output left
         # eye position, and desired output face width + height
@@ -18,33 +75,52 @@ class FaceAligner:
         self.desiredLeftEye = desiredLeftEye
         self.desiredFaceWidth = desiredFaceWidth
         self.desiredFaceHeight = desiredFaceHeight
+
+        # dictionary that maps the indexes of the facial
+        # landmarks to specific face region
+        #For dlib’s 68-point facial landmark detector:
+        self.FACIAL_LANDMARKS_IDXS = OrderedDict([
+        	("mouth", (48, 68)),
+        	("inner_mouth", (60, 68)),
+        	("right_eyebrow", (17, 22)),
+        	("left_eyebrow", (22, 27)),
+        	("right_eye", (36, 42)),
+        	("left_eye", (42, 48)),
+        	("nose", (27, 36)),
+        	("jaw", (0, 17))
+        ])
  
         # if the desired face height is None, set it to be the
         # desired face width (normal behavior)
         if self.desiredFaceHeight is None:
             self.desiredFaceHeight = self.desiredFaceWidth
-    def align(self, image, gray, rect):
-        # convert the landmark (x, y)-coordinates to a NumPy array
-        shape = self.predictor(gray, rect)
-        shape = shape_to_np(shape)
- 
-        # extract the left and right eye (x, y)-coordinates
-        (lStart, lEnd) = FACIAL_LANDMARKS_IDXS["left_eye"]
-        (rStart, rEnd) = FACIAL_LANDMARKS_IDXS["right_eye"]
+
+    def convertOpencvMatToDLibRect (self,image):
+        image = resize(image, width=800)
+        (he,wi)=image.shape[:2]
+        rect = dlib.rectangle( left= 0, top= 0, right=800, bottom= he)
+        return image,rect
+
+    def extractEyesCoordinates (self,shape):
+        (lStart, lEnd) = self.FACIAL_LANDMARKS_IDXS["left_eye"]
+        (rStart, rEnd) = self.FACIAL_LANDMARKS_IDXS["right_eye"]
         leftEyePts = shape[lStart:lEnd]
         rightEyePts = shape[rStart:rEnd]
-        # compute the center of mass for each eye
+        return leftEyePts,rightEyePts
+
+    def getCenterOfMassOfEyes (self,leftEyePts,rightEyePts):
         leftEyeCenter = leftEyePts.mean(axis=0).astype("int")
         rightEyeCenter = rightEyePts.mean(axis=0).astype("int")
- 
+        return leftEyeCenter,rightEyeCenter
+
+    def getAngleOfEyeCentroids (self,leftEyeCenter,rightEyeCenter):
         # compute the angle between the eye centroids
         dY = rightEyeCenter[1] - leftEyeCenter[1]
         dX = rightEyeCenter[0] - leftEyeCenter[0]
         angle = np.degrees(np.arctan2(dY, dX)) - 180
-        # compute the desired right eye x-coordinate based on the
-        # desired x-coordinate of the left eye
-        desiredRightEyeX = 1.0 - self.desiredLeftEye[0]
- 
+        return angle,dX,dY
+
+    def getNewScaleOfImage (self,dX,dY,desiredRightEyeX):
         # determine the scale of the new resulting image by taking
         # the ratio of the distance between eyes in the *current*
         # image to the ratio of distance between eyes in the
@@ -53,6 +129,31 @@ class FaceAligner:
         desiredDist = (desiredRightEyeX - self.desiredLeftEye[0])
         desiredDist *= self.desiredFaceWidth
         scale = desiredDist / dist
+        return scale
+
+
+    # image_small : The RGB input image.
+    # gray : The grayscale input image.
+    # rect : The bounding box rectangle produced by dlib’s HOG face detector.
+    def align(self, image_small):
+        image,rect = self.convertOpencvMatToDLibRect (image_small)
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        shape = self.predictor(gray, rect)
+        shape = shape_to_np(shape)
+
+        leftEyePts,rightEyePts = self.extractEyesCoordinates(shape)
+
+        leftEyeCenter,rightEyeCenter = self.getCenterOfMassOfEyes(leftEyePts,rightEyePts)
+ 
+        angle,dX,dY = self.getAngleOfEyeCentroids (leftEyeCenter,rightEyeCenter)
+
+        # compute the desired right eye x-coordinate based on the
+        # desired x-coordinate of the left eye
+        desiredRightEyeX = 1.0 - self.desiredLeftEye[0]
+ 
+        scale = self.getNewScaleOfImage (dX,dY,desiredRightEyeX)
+
         # compute center (x, y)-coordinates (i.e., the median point)
         # between the two eyes in the input image
         eyesCenter = ((leftEyeCenter[0] + rightEyeCenter[0]) // 2,
--- faceDetection.py
+++ faceDetection.py
@@ -5,29 +5,19 @@
 # GPUs. It is better than the face_recognition module from Dlib Geitgey
 # only because it recognizes faces smaller than 80x80px.
 
-
-
-
-#import libraries
 import os   
-#print(os.__file__)
 import cv2
 import numpy
 import sys
 import platform
-#import dlib
-#import dlibFaceRecognition
 import pprint
 import math
 import pickle
 
 import FaceRecognitionUtils as fr
 import ObjectTracker
+import FaceAligner
 
-
-#geyguy face_recognition library globals
-known_face_encodings = []
-knon_face_metadata = []
 usingplatform = ""
 
 def Init ():
@@ -65,7 +55,6 @@ def GetVideoObject():
             print("Cannot find camera, quitting")
             sys.exit()
     elif usingplatform  == "AMD64": 
-        #print("Running on a Windows System")
         #video_capture = cv2.VideoCapture(0+cv2.CAP_DSHOW)
         #video_capture = cv2.VideoCapture(1+cv2.CAP_DSHOW)
         video_capture = cv2.VideoCapture(1+cv2.CAP_DSHOW)
@@ -102,75 +91,8 @@ def OpenCVUsingCuda():
         print("Could Not Find CUDA")
 
 def CaptureVideo():
-    net_model = LoadPretrainedFaceRecognitionModel()
     video_capture = GetVideoObject()
-    process_this_frame = 0
-    trackingBoundingBox = None
-    trackerReady = False
-    box = None
-    trackerr = None
-    success= None
-    name = ""
-    recognizer = fr.FaceRecognizer()
-
-    #trackerr = make_myTracker(0,0,0,0)
-    listOfTrackedFaces = []
-
-    while True: 
-        # process each frame
-        ret, frame = video_capture.read()
-        if ret == False:
-            continue
-
-        blob = GetBlobFromFrame(frame)
-        if process_this_frame >= 16:
-            process_this_frame = 0
-
-        if process_this_frame == 0 :
-            trackingBoundingBox = None
-            box = None
-            del listOfTrackedFaces[:]
-
-            mat_detections = ExtractDetectedFaces(blob,net_model)
-            for i in range(0, mat_detections.shape[2]):
-                # only grab faces >60% detected confidence
-                if mat_detections[0, 0, i, 2] > 0.6:
-                    (h,w) = frame.shape[:2];
-                    box = mat_detections[0, 0, i, 3:7] * numpy.array([w, h, w, h])
-                    (startX, startY, endX, endY) = box.astype("int")
-                    confidence = mat_detections[0, 0, i, 2]
-                    face_image = frame[startY:endY, startX:endX]
-                    if face_image.size is 0: #frame on error returns an emtpy image
-                        continue
-                    # draw square around detected
-                    cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 0, 0), 2)
-                    trackerr = None
-                    trackerr = ObjectTracker.make_myTracker(startX,startY,endX,endY)
-                    trackerr.start(frame)
-                    proba,name = recognizer.RecognizeFromFace (face_image)
-                    mytuple = (proba,name,trackerr)
-                    listOfTrackedFaces.append(mytuple)
-
-        process_this_frame += 1
-        for item in range(len(listOfTrackedFaces)):
-            proba,name,trackerr = listOfTrackedFaces[item]
-            (success, trackingBoundingBox) = trackerr.update(frame)
-            # check to see if the tracking was a success
-            if success:
-               process_this_frame += 1
-               text = "{}: {:.2f}% item:{}".format(name, proba * 100,item)
-               (x, y, w, h) = [int(v) for v in trackingBoundingBox]
-               cv2.rectangle(frame, (x-1, y), (x + w, y + h),(0, 255, 0), 2)
-               cv2.putText(frame,text,(x,y-1),cv2.FONT_HERSHEY_SIMPLEX,0.45,(0,255,0),2)
-
-        # Display frame 
-        cv2.imshow('Video', frame)
-        # Hit 'q' on the keyboard to quit
-        keypressed = 0xFF & cv2.waitKey(1)
-        if keypressed == ord('q') or keypressed == ord('Q') :
-           break
-
-        continue;
+    return video_capture
 
 def GetBlobFromFrame(frame):
     #get our blob which is our input image after mean subtraction, normalizing, and channel swapping
@@ -208,71 +130,85 @@ def GetNewHeightMaintainRatio (desiredWidth,startX,endX,startY,endY):
     newHeight = int(desiredWidth/faceAspectRatio)
     return newHeight
 
-def DrawBox(mat_detections,frame,i,count):
-    (h,w) = frame.shape[:2];
-    box = mat_detections[0, 0, i, 3:7] * numpy.array([w, h, w, h])
-    (startX, startY, endX, endY) = box.astype("int")
-
-    confidence = mat_detections[0, 0, i, 2]
-
-    cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 0, 0), 2)
-    facestr = 'Conf: {0:.{1}},Faces: {2}, pos:{3}'.format(  confidence,2, count,startX)
-
-    #cv2.putText(frame, facestr , (startX + 6, startY - 6), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1)
-    #Draw the detected face
-    newWidth = 96
-    newHeight = GetNewHeightMaintainRatio (newWidth,startX,endX,startY,endY)#int(newWidth/faceAspectRatio)
-
-    face_image = frame[startY:endY, startX:endX] 
-
-    if count == 1:
-        xPos = 0
-    else:
-        xPos = (count-1)*newWidth
-
-    if face_image.any() and count > 0:
-        face_image = cv2.resize(face_image, (newWidth, newHeight))
-        frame[30:newHeight+30, xPos:xPos + newWidth] = face_image
-        # Some images have no size and create an error
-        # so make sure you run the embedding after a resizing.
-        #qembeddingVector = fr.CreateFaceEmbedding (face_image,embedder)
-
-        #send face to face recog engine
-        vec = fr.CreateFaceEmbedding (face_image,embedder)
-        vec = embedder.forward()
-        # perform classification to recognize the face
-        preds = recognizer.predict_proba(vec)[0]
-        j = numpy.argmax(preds)
-        proba = preds[j]
-        name = le.classes_[j]
-        if proba > 0.70:
-
-            # draw the bounding box of the face along with the
-            # associated probability
-            text = "{}: {:.2f}%".format(name, proba * 100)
-            y = startY - 10 if startY - 10 > 10 else startY + 10
-            cv2.rectangle(frame, (startX, startY), (endX, endY),
-                (0, 0, 255), 2)
-            cv2.putText(frame, text, (startX, y),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-    if name is None:
-        name = " "
-    return face_image,name
-
-def IterateOverDetectedFaces (mat_detections,frame):
-    count = 0
-    for i in range(0, mat_detections.shape[2]):
-        # only grab faces with 60% confidence or more
-        if mat_detections[0, 0, i, 2] > 0.6:
-            count +=1
-            face_box = DrawBox(mat_detections,frame,i,count)
-
-
 def main():
     Init()
-    CaptureVideo()
+    net_model = LoadPretrainedFaceRecognitionModel()
+    video_capture = CaptureVideo()
+
+    process_this_frame = 0
+    trackingBoundingBox = None
+    trackerReady = False
+    box = None
+    trackerr = None
+    success= None
+    name = ""
+    recognizer = fr.FaceRecognizer()
+    fa = FaceAligner.FaceAligner(desiredFaceWidth=256)
+
+    #trackerr = make_myTracker(0,0,0,0)
+    listOfTrackedFaces = []
+
+    while True: 
+        # process each frame
+        ret, frame = video_capture.read()
+        if ret == False:
+            continue
+
+        blob = GetBlobFromFrame(frame)
+        if process_this_frame >= 16:
+            process_this_frame = 0
+
+        if process_this_frame == 0 :
+            trackingBoundingBox = None
+            box = None
+            del listOfTrackedFaces[:]
+
+            mat_detections = ExtractDetectedFaces(blob,net_model)
+            for i in range(0, mat_detections.shape[2]):
+                # only grab faces >60% detected confidence
+                if mat_detections[0, 0, i, 2] > 0.6:
+                    (h,w) = frame.shape[:2];
+                    box = mat_detections[0, 0, i, 3:7] * numpy.array([w, h, w, h])
+                    (startX, startY, endX, endY) = box.astype("int")
+                    confidence = mat_detections[0, 0, i, 2]
+                    face_image = frame[startY:endY, startX:endX]
+                    if face_image.size is 0: #frame on error returns an emtpy image
+                        continue
+                    # draw square around detected
+                    cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 0, 0), 2)
+                    trackerr = None
+                    trackerr = ObjectTracker.make_myTracker(startX,startY,endX,endY)
+                    trackerr.start(frame)
+                    faceAligned = fa.align(face_image)
+                    #faceAligned = face_image
+
+                    proba,name = recognizer.RecognizeFromFace (face_image)
+                    mytuple = (proba,name,trackerr,faceAligned)
+                    listOfTrackedFaces.append(mytuple)
+
+        process_this_frame += 1
+        for item in range(len(listOfTrackedFaces)):
+            proba,name,trackerr,just_the_face = listOfTrackedFaces[item]
+            (success, trackingBoundingBox) = trackerr.update(frame)
+            # check to see if the tracking was a success
+            if success:
+               process_this_frame += 1
+               text = "{}: {:.2f}% item:{}".format(name, proba * 100,item)
+               (x, y, w, h) = [int(v) for v in trackingBoundingBox]
+               cv2.rectangle(frame, (x-1, y), (x + w, y + h),(0, 255, 0), 2)
+               cv2.putText(frame,text,(x,y-1),cv2.FONT_HERSHEY_SIMPLEX,0.45,(0,255,0),2)
+               #frame[30:newHeight+30, xPos:xPos + newWidth] = just_the_face
+
+        # Display frame 
+        cv2.imshow('Video', frame)
+        # Hit 'q' on the keyboard to quit
+        keypressed = 0xFF & cv2.waitKey(1)
+        if keypressed == ord('q') or keypressed == ord('Q') :
+           break
+
+        continue;
+
 
 if __name__ == "__main__":
    main()
-
 
